d7b12a60d80e43428fd7e2e1f6c00a0ea77d9221
attabasd <dsabatta@gmail.com>
2012-06-20 08:52:48 +0200

    Changes to hashfiles.py

--- hashfiles.py
+++ hashfiles.py
@@ -10,7 +10,7 @@ def hashfile(theFile, hasher, blocksize=65536):
 		buf = theFile.read(blocksize);
 	return binascii.hexlify(hasher.digest()).upper();
 
-
-fileList = {'README.md', 'hashfiles.py'};
-print [(fname, hashfile(file(fname, 'r'), hashlib.md5())) for fname in fileList];
+if __name__ == "__main__":
+	fileList = {'README.md', 'hashfiles.py'};
+	print [(fname, hashfile(file(fname, 'r'), hashlib.md5())) for fname in fileList];
 
